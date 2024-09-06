f4c7b335e77e477c2a220c8896a4582a3911fba4
Lee <leaveil.johnson1881@gmail.com>
2024-09-06 19:36:45 +0200

    Run script every 10 seconds

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import selectorlib
 import smtplib
 import ssl
 import certifi
+import time
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"}
@@ -43,11 +44,13 @@ def read(extracted):
 
 
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
-    content = read(extracted)
-    if extracted != "No upcoming tours":
-        if extracted not in content:
-            store(extracted)
-            send_email()
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_email(message="New event found!")
+        time.sleep(10)
